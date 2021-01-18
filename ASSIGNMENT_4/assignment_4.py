c8c801a36926257f6d2dcfbcccf9c4fe04e51751
annu100 <75775352+annu100@users.noreply.github.com>
2021-01-18 16:44:24 +0530

    Add files via upload

---
+++ ASSIGNMENT_4/assignment_4.py
@@ -0,0 +1,21 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/mirtynjay kumar/Desktop/python codes/Assign_4_no_of_trials_bernaulli.py
+Probability for getting atleast 1 head for first guess
+0.75 
+
+Probability for getting atleast one head for second guess
+0.875 
+
+Probability for getting atleast 1 head for third guess
+0.9375 
+
+Probability for getting atleast 1 head for fourth guess
+0.96875 
+
+Probability for getting atleast 1 head for fifth guess
+0.984375 
+
+from the graph ,we can easily see that for no. of trails greater than or equal to 4 ,the probability of getting atleast one head is greater than 0.9
+>>> 
