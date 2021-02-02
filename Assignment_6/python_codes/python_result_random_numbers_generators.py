121db2a805b0416ed7683de90d6e17111c3940c1
annu100 <75775352+annu100@users.noreply.github.com>
2021-02-02 13:25:12 +0530

    Add files via upload

---
+++ Assignment_6/python_codes/python_result_random_numbers_generators.py
@@ -0,0 +1,20 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/mirtynjay kumar/Desktop/Probability_RandomVariales/Assignment_6/assignment_6_random no. generator.py
+Enter number of samples to be printed a/c to pmf
+100
+Generated samples according to the probability distribution is 
+
+['2', '2', '2', '1', '0', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '1', '1', '2', '2', '2', '2', '2', '2', '2', '0', '2', '2', '2', '0', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '1', '2', '2', '2', '2', '2', '2', '2', '1', '1', '2', '2', '2', '2', '1', '1', '2', '2', '2', '2', '1', '2', '2', '2', '2', '2', '2', '2', '2', '2', '1', '0', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '1', '2', '2', '2', '2', '2', '2', '2']
+4
+11
+85
+simulation versus actual probabilities
+Actual probabilities are [0.04, 0.08, 0.88]
+simulation probabilities are given by 
+
+Probability for X=0 is  0.04
+Probability for X=1 is  0.11
+Probability for X=2 is  0.85
+>>> 
