4ee004a6e33abfbb1d010639b757755d58cfb63c
annu100 <75775352+annu100@users.noreply.github.com>
2021-02-25 23:18:11 +0530

    Update result.py

--- Assignment7/python_codes/result.py
+++ Assignment7/python_codes/result.py
@@ -2,8 +2,8 @@ Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (In
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
 = RESTART: C:/Users/mirtynjay kumar/Desktop/Probability_RandomVariales/Assignment7/python_codes/simultion_2_gaussian.py
-mean and var of sample_X are:  0.022985868954109877 and 0.3063122634913793
-mean and var of sample_Y are:  -0.00632865937923826 and 0.29473529196652387
+mean and var of sample_X are:  0.002985868954109877 and 0.3063122634913793
+mean and var of sample_Y are:  0.00632865937923826 and 0.29473529196652387
 sum of individual means and variances of simulated X and Y:  0.01665720957487162 and 0.6010475554579031
-mean and var of transformed R.V. U are:  0.016657209574871615 0.5954568605065856
+mean and var of transformed R.V. U are:  0.016657209574871615 0.6010475554579031
 >>> 
